e4c087560f713e475769f5ba9ac6cbfd9ccf2521
sersim33 <sersim33@gmail.com>
2024-05-18 19:14:05 +0300

    com5

--- task4.py
+++ task4.py
@@ -1,3 +1,5 @@
+import uuid
+import heapq
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -7,47 +9,57 @@ class Node:
         self.right = None
         self.val = key
         self.color = color  # Додатковий аргумент для зберігання кольору вузла
-        self.id = str(id(self))  # Унікальний ідентифікатор для кожного вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
 
-def draw_heap(heap_root):
-    tree = nx.DiGraph()
-    pos = {}
-    index = 0
+def build_heap(data):
+    heapq.heapify(data)
+    return data
 
-    # Додаємо кореневий вузол
-    pos[heap_root.id] = (0, 0)
-    tree.add_node(heap_root.id, label=str(heap_root.val), color=heap_root.color)
+def list_to_tree(data, index=0):
+    """ Перетворює список у бінарне дерево. """
+    if index < len(data):
+        node = Node(data[index])
+        node.left = list_to_tree(data, 2 * index + 1)
+        node.right = list_to_tree(data, 2 * index + 2)
+        return node
+    return None
 
-    # Додаємо інші вузли та ребра
-    queue = [heap_root]
-    while queue:
-        node = queue.pop(0)
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
         if node.left:
-            index += 1
-            pos[node.left.id] = (pos[node.id][0] - 1 / (2 ** index), pos[node.id][1] - 1)
-            tree.add_node(node.left.id, label=str(node.left.val), color=node.left.color)
-            tree.add_edge(node.id, node.left.id)
-            queue.append(node.left)
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
-            index += 1
-            pos[node.right.id] = (pos[node.id][0] + 1 / (2 ** index), pos[node.id][1] - 1)
-            tree.add_node(node.right.id, label=str(node.right.val), color=node.right.color)
-            tree.add_edge(node.id, node.right.id)
-            queue.append(node.right)
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
 
-    # Відображаємо купу
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
     colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
 
-    plt.figure(figsize=(10, 7))
-    nx.draw(tree, pos=pos, labels=labels, arrows=True, node_size=2000, node_color=colors, font_size=12, font_weight='bold')
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
     plt.show()
 
-# Приклад використання
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+# Дані для побудови купи
+data = [10, 4, 9, 1, 7, 5, 3, 55]
 
-draw_heap(root)
+# Побудова бінарної купи
+heap_data = build_heap(data)
+print("Heapified data:", heap_data)
+
+# Перетворення списку в дерево
+root = list_to_tree(heap_data)
+
+# Відображення дерева
+draw_tree(root)
--- task5.py
+++ task5.py
@@ -1,94 +1,115 @@
+import uuid
 import heapq
-import colorsys
 import networkx as nx
 import matplotlib.pyplot as plt
-from collections import deque  # Importing deque from collections module
+from collections import deque
+from matplotlib.colors import LinearSegmentedColormap
 
 class Node:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
 
-def add_edges(tree, node, pos):
-    if node.left:
-        tree.add_edge(node.id, node.left.id)
-        pos[node.left.id] = (pos[node.id][0] - 1, pos[node.id][1] - 1)
-        tree = add_edges(tree, node.left, pos)
-    if node.right:
-        tree.add_edge(node.id, node.right.id)
-        pos[node.right.id] = (pos[node.id][0] + 1, pos[node.id][1] - 1)
-        tree = add_edges(tree, node.right, pos)
-    return tree
+def build_heap(data):
+    heapq.heapify(data)
+    return data
 
+def list_to_tree(data, index=0):
+    """ Перетворює список у бінарне дерево. """
+    if index < len(data):
+        node = Node(data[index])
+        node.left = list_to_tree(data, 2 * index + 1)
+        node.right = list_to_tree(data, 2 * index + 2)
+        return node
+    return None
 
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
 
-def depth_first_traversal(node, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(node)
-    yield node
-
-    if node.left and node.left not in visited:
-        yield from depth_first_traversal(node.left, visited)
-    
-    if node.right and node.right not in visited:
-        yield from depth_first_traversal(node.right, visited)
-
-def breadth_first_traversal(root):
-    if root is None:
-        return
-    
-    queue = deque([root])
-    visited = set([root])
-    while queue:
-        node = queue.popleft()
-        yield node
-        if node.left and node.left not in visited:
-            queue.append(node.left)
-            visited.add(node.left)
-        if node.right and node.right not in visited:
-            queue.append(node.right)
-            visited.add(node.right)
-
-def draw_tree_with_traversals(tree_root):
-    # Фіксований відтінок зеленого
-    hue = 0.3
-    # Обійти дерево у глибину та присвоїти кожному вузлу унікальний колір
-    dfs_traversal = list(depth_first_traversal(tree_root))
-    dfs_colors = {node.id: colorsys.hsv_to_rgb(hue, 1.0, (i + 1) / len(dfs_traversal)) for i, node in enumerate(dfs_traversal)}
-    # Обійти дерево у ширину та присвоїти кожному вузлу унікальний колір
-    bfs_traversal = list(breadth_first_traversal(tree_root))
-    bfs_colors = {node.id: colorsys.hsv_to_rgb(hue, 1.0, (i + 1) / len(bfs_traversal)) for i, node in enumerate(bfs_traversal)}
-
+def draw_tree(tree_root):
     tree = nx.DiGraph()
     pos = {tree_root.id: (0, 0)}
     tree = add_edges(tree, tree_root, pos)
 
-    colors = [dfs_colors[node[0]] for node in tree.nodes(data=True)]  # Кольори за глибинним обходом
-    labels = {node: node for node in tree.nodes()}
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
 
-
-    plt.figure(figsize=(16, 8))
-
-    plt.subplot(1, 2, 1)
+    plt.figure(figsize=(8, 5))
     nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.title("Depth First Traversal")
-
-    colors = [bfs_colors[node[0]] for node in tree.nodes(data=True)]  # Кольори за обходом у ширину
-
-    plt.subplot(1, 2, 2)
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.title("Breadth First Traversal")
-
     plt.show()
 
-# Використання:
-root = Node(0)
-root.left = Node(4)
-root.left.left = Node(5)
-root.left.right = Node(10)
-root.right = Node(1)
-root.right.left = Node(3)
+def dfs(node, order):
+    if node is None:
+        return
+    order.append(node)
+    dfs(node.left, order)
+    dfs(node.right, order)
 
-draw_tree_with_traversals(root)
+def bfs(root):
+    if root is None:
+        return []
+    queue = deque([root])
+    order = []
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return order
+
+def apply_colors(order, cmap):
+    n = len(order)
+    colors = [cmap(i / n) for i in range(n)]
+    for node, color in zip(order, colors):
+        node.color = color
+
+# Дані для побудови купи
+data = [10, 4, 9, 1, 7, 5, 3]
+
+# Побудова бінарної купи
+heap_data = build_heap(data)
+print("Heapified data:", heap_data)
+
+# Перетворення списку в дерево
+root = list_to_tree(heap_data)
+
+# Обхід в глибину (DFS) та призначення кольорів
+dfs_order = []
+dfs(root, dfs_order)
+
+# Обхід в ширину (BFS) та призначення кольорів
+bfs_order = bfs(root)
+
+# Створення кольорової карти від темних до світлих відтінків
+cmap = LinearSegmentedColormap.from_list("custom_cmap", ["darkblue", "skyblue"])
+
+# Застосування кольорів до вузлів дерева на основі DFS
+apply_colors(dfs_order, cmap)
+print("DFS traversal:", [node.val for node in dfs_order])
+
+# Відображення дерева з кольорами для обходу DFS
+draw_tree(root)
+
+# Застосування кольорів до вузлів дерева на основі BFS (для порівняння)
+apply_colors(bfs_order, cmap)
+print("BFS traversal:", [node.val for node in bfs_order])
+
+# Відображення дерева з кольорами для обходу BFS (заново відобразити дерево)
+draw_tree(root)
